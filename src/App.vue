dceac4f0176f790e51fcbc15e3342d4b721c2a02
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-09 12:16:06 +1200

    Update preview banner as a standalone component

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,12 @@ import BackToTop from '@/components/atoms/BackToTop.vue'
 import BetaRibbon from '@/components/atoms/BetaRibbon.vue'
 import CookieBanner from '@/components/molecules/CookieBanner.vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
-import NotificationBar from './components/molecules/NotificationBar.vue';
-import ActionButton from '@/components/atoms/ActionButton.vue'
+import PreviewBanner from '@/components/molecules/PreviewBanner.vue'
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
-    <NotificationBar>
-      You are viewing a preview version of PMR.
-      <ActionButton
-        href="#"
-        variant="secondary"
-        size="sm"
-      >
-        View classic site
-      </ActionButton>
-      <ActionButton
-        to="/feature-comparison"
-        variant="primary"
-        size="sm"
-      >
-        Compare features
-      </ActionButton>
-    </NotificationBar>
+    <PreviewBanner />
     <DefaultLayout>
       <RouterView />
     </DefaultLayout>
