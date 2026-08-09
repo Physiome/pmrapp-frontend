d1dc58437955cf4eb6fb4ec06083cef053c69a51
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-10 11:48:18 +1200

    Fix type errors

--- src/components/molecules/FeatureComparison.vue
+++ src/components/molecules/FeatureComparison.vue
@@ -127,8 +127,8 @@ onMounted(() => {
           </div>
         </li>
         <li
-          v-for="row in group.rows"
-          :key="row.id"
+          v-for="(row, index) in group.rows"
+          :key="row.id ?? index"
           class="hover:bg-gray-50 dark:hover:bg-gray-800 transition-colors"
           role="row"
         >
