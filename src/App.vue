c822f16329b35edcbabb3e170f394c98386054dc
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-09 12:03:38 +1200

    Add preview banner

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,29 @@ import BackToTop from '@/components/atoms/BackToTop.vue'
 import BetaRibbon from '@/components/atoms/BetaRibbon.vue'
 import CookieBanner from '@/components/molecules/CookieBanner.vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import NotificationBar from './components/molecules/NotificationBar.vue';
+import ActionButton from '@/components/atoms/ActionButton.vue'
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
+    <NotificationBar>
+      You are viewing a preview version of PMR.
+      <ActionButton
+        href="#"
+        variant="secondary"
+        size="sm"
+      >
+        View classic site
+      </ActionButton>
+      <ActionButton
+        to="/feature-comparison"
+        variant="primary"
+        size="sm"
+      >
+        Compare features
+      </ActionButton>
+    </NotificationBar>
     <DefaultLayout>
       <RouterView />
     </DefaultLayout>
--- src/components/molecules/NotificationBar.vue
+++ src/components/molecules/NotificationBar.vue
@@ -25,10 +25,7 @@ onMounted(async () => {
     class="bg-amber-100 dark:bg-amber-900/20 border-b border-amber-300 dark:border-amber-700 relative"
   >
     <div class="container mx-auto px-4 pr-10 lg:pr-4 py-2 flex items-center justify-center gap-2 text-sm text-amber-900 dark:text-amber-100">
-      <span>⚠️</span>
-      <span>
-        This is a general notification message.
-      </span>
+      <slot />
     </div>
     <CloseButton
       class="absolute top-1/2 right-4 -translate-y-1/2"
