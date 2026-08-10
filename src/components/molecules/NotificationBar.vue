a596d1079b673845de44e9fa27cb34e59f266512
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-11 10:50:36 +1200

    Update notification banner close button style

--- src/components/molecules/NotificationBar.vue
+++ src/components/molecules/NotificationBar.vue
@@ -28,7 +28,7 @@ onMounted(async () => {
       <slot />
     </div>
     <CloseButton
-      class="absolute top-1/2 right-4 -translate-y-1/2"
+      class="absolute top-1/2 right-4 -translate-y-1/2 w-6 h-6 rounded hover:bg-gray-100 dark:hover:bg-gray-800 transition"
       @click="handleClose"
       aria-label="Close notification"
     />
