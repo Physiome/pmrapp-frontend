4032c4248a13824b3a35225a86afd80a4417aa55
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-10 17:09:00 +1200

    Create issue directly on clicking the bug report button

--- src/components/atoms/FeedbackButton.vue
+++ src/components/atoms/FeedbackButton.vue
@@ -2,11 +2,21 @@
 import BugIcon from '@/components/icons/BugIcon.vue'
 import { GITHUB_ISSUES_URL } from '@/constants/global'
 import ActionButton from './ActionButton.vue'
+
+/**
+ * Build the GitHub "new issue" URL with the current page URL
+ * prefilled in the issue body.
+ */
+const newIssueUrl = (): string => {
+  const currentUrl = window.location.href
+  const body = `**Page:** ${currentUrl}\n\n`
+  return `${GITHUB_ISSUES_URL}/new?title=&body=${encodeURIComponent(body)}`
+}
 </script>
 
 <template>
   <ActionButton
-    :href="GITHUB_ISSUES_URL"
+    :href="newIssueUrl()"
     target="_blank"
     rel="noopener noreferrer"
     variant="icon"
