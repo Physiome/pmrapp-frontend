7eb7f33de62f3261ce4e68fded40fe426e088bb9
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-13 14:40:52 +1200

    Add warning box component

---
+++ src/components/molecules/WarningBlock.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  message: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="warning-box">
+    <h3 class="font-semibold mb-2">{{ title }}</h3>
+    <p class="text-sm">{{ message }}</p>
+    <div v-if="$slots.default" class="mt-4 flex flex-wrap gap-2">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '@/assets/warning-box.css';
+</style>
